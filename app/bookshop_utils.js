758fdfd25a30436d7682f808d1b71ba2e28be781
Shazia <n_kirmani@yahoo.com>
2019-10-07 21:38:06 +0100

    Two new tests ready for review

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,23 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
+addVAT("Hello")
+
+
+
+function getFullName(a , b) {
+     const fullName = (a+ ' '+ b)
+     return fullName
+}
+function makeHalfPrice(price) {
+    
+    return price - (0.5*price)
+}
 
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+
 };
+
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,21 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+describe("utils.countBooks", () => {
+  test("returns 50 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  
+  
+  
+  });
+
 });
