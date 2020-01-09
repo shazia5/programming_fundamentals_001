949ba204d96909cd36173189916a674d0109f71a
Shazia <n_kirmani@yahoo.com>
2020-01-09 13:34:55 +0000

    was not pushed

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,23 +1,7 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
-addVAT("Hello")
-
-
-
-function getFullName(a , b) {
-     const fullName = (a+ ' '+ b)
-     return fullName
-}
-function makeHalfPrice(price) {
-    
-    return price - (0.5*price)
-}
 
 module.exports = {
-    addVAT,
-    getFullName,
-    makeHalfPrice,
-
+    addVAT
 };
-
